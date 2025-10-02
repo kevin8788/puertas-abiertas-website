71d763fee62f5cdf50c73c477399734de7ed034b
kevin <kevinmendoza30@yahoo.com>
2025-10-01 22:14:48 -0700

    f

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
-import HomePage from "@/pages/HomePage";
+
 import { Metadata } from "next";
 import Image from "next/image";
+import HomePage from "./pages/HomePage";
 
 export const metadata: Metadata = {
   title: 'Iglesia Puertas Abiertas Home'
